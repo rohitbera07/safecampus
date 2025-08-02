d219fabf829f1a1d365592cb00f3ca4330b9b7d3
rohitbera07 <rohitberaofficial07@gmail.com>
2025-08-02 12:32:42 +0530

    Fix: resolved

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Lottie from 'lottie-react';
-import animationData from '../assets/animations/1MpOlcwm93.json'
+
 import { useNavigate } from 'react-router-dom';
 
 const Hero = () => {
